a53dc9299a61bec1fd47c6ca5babae0be2569331
Adeleke Ayinde <lekea4@gmail.com>
2021-10-13 16:31:31 +0100

    Modify the ICandidateRepositoy class to include the Update action

--- Testr.Domain/Interfaces/ICandidateRepository.cs
+++ Testr.Domain/Interfaces/ICandidateRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Testr.Domain.DTOs;
 using Testr.Domain.Entities;
 using Testr.Domain.Interfaces.Base;
 
@@ -7,5 +8,6 @@ namespace Testr.Domain.Interfaces
     public interface ICandidateRepository : IRepository<Candidate>
     {
         public Task AddAsync(CandidateRegistrationDTO candidateInfo, ApplicationUser userInfo);
+        public Task UpdateAsync(CandidateUpdateProfileDTO candidateProfileUpdate, long id);
     }
 }
